15e410407799c30ba75f989e6078c2588a0d64aa
Khamoosh <gu.khamoosh@gmail.com>
2025-07-15 06:59:14 +0430

    Add files via upload
    // دریافت داده‌های فرم و نمایش پیغام موفقیت بدون ارسال واقعی

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
-function searchImages() {
-  const input = document.getElementById('searchInput').value.toLowerCase();
-  const images = document.querySelectorAll('#gallery img');
-  images.forEach(img => {
-    const altText = img.alt.toLowerCase();
-    if (altText.includes(input)) {
-      img.style.display = 'inline';
-    } else {
-      img.style.display = 'none';
-    }
-  });
-}
+
+document.getElementById("contactForm").addEventListener("submit", function(e){
+    e.preventDefault();
+    alert("پیام شما ارسال شد!");
+});
+    
