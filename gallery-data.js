05bb06993d653608b1bd0d49782519e9cc45272f
Khamoosh <gu.khamoosh@gmail.com>
2025-07-15 16:53:26 +0430

    Add files via upload
    // comment change: گالری - لیست تصاویر

---
+++ gallery-data.js
@@ -0,0 +1,10 @@
+
+// comment change: گالری - لیست تصاویر
+const images = [
+  { src: 'images/sample1.jpg', alt: 'تصویر نمونه ۱' },
+  { src: 'images/sample2.jpg', alt: 'تصویر نمونه ۲' },
+  { src: 'images/sample3.jpg', alt: 'تصویر نمونه ۳' },
+  { src: 'images/sample4.jpg', alt: 'تصویر نمونه ۴' },
+  { src: 'images/sample5.jpg', alt: 'تصویر نمونه ۵' },
+  { src: 'images/sample6.jpg', alt: 'تصویر نمونه ۶' }
+];
