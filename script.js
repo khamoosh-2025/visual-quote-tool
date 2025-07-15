52f8089b991cc44a912c3b9930dc799d85a4d297
Khamoosh <gu.khamoosh@gmail.com>
2025-07-15 17:32:36 +0430

    Add files via upload
    Merge popup and watermark versions into final interactive gallery with custom text and position - gallery-popup-final.zip

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 
+function openPopup(imgElement) {
+    const popup = document.getElementById("popup");
+    const popupImg = document.getElementById("popup-img");
+    const caption = document.getElementById("caption");
+    popup.style.display = "flex";
+    popupImg.src = imgElement.src;
+    caption.innerText = imgElement.alt;
+}
+
+function closePopup() {
+    document.getElementById("popup").style.display = "none";
+}
+
 function applyWatermarks() {
     const text = document.getElementById('watermarkText').value;
     const position = document.getElementById('position').value;
