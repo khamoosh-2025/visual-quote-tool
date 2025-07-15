fbfd47e93d0635da4f87b7308fb377f2aa2a9487
Khamoosh <gu.khamoosh@gmail.com>
2025-07-15 13:51:35 +0430

    Add files via upload
    // gallery.js | برای تعاملات آینده و بارگذاری اسکریپت

--- gallery.js
+++ gallery.js
@@ -1,13 +1,2 @@
-function addCustomImage() {
-  const input = document.getElementById('uploadInput');
-  const file = input.files[0];
-  if (file) {
-    const reader = new FileReader();
-    reader.onload = function (e) {
-      const img = document.createElement('img');
-      img.src = e.target.result;
-      document.getElementById('gallery').appendChild(img);
-    };
-    reader.readAsDataURL(file);
-  }
-}
+// Gallery JS – reserved for future enhancements
+console.log("گالری بارگذاری شد");
