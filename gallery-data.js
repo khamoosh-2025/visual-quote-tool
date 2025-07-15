19d660381cd5f6ba635a3ba05df5d8bcdd13d264
Khamoosh <gu.khamoosh@gmail.com>
2025-07-15 17:00:22 +0430

    Add files via upload
    // دریافت تصاویر ذخیره‌شده از localStorage هنگام بارگذاری صفحه

--- gallery-data.js
+++ gallery-data.js
@@ -1,10 +1,3 @@
-
-// comment change: گالری - لیست تصاویر
-const images = [
-  { src: 'images/sample1.jpg', alt: 'تصویر نمونه ۱' },
-  { src: 'images/sample2.jpg', alt: 'تصویر نمونه ۲' },
-  { src: 'images/sample3.jpg', alt: 'تصویر نمونه ۳' },
-  { src: 'images/sample4.jpg', alt: 'تصویر نمونه ۴' },
-  { src: 'images/sample5.jpg', alt: 'تصویر نمونه ۵' },
-  { src: 'images/sample6.jpg', alt: 'تصویر نمونه ۶' }
-];
+// gallery-data.js
+// ذخیره تصاویر در حافظه مرورگر
+const storedImages = JSON.parse(localStorage.getItem('images')) || [];
